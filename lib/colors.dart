4e727e34dc89d6fba519b2e383485569595807a6
Christian Tsoungui Nkoulou <ChristianN@ruthsx.com>
2021-06-09 23:45:49 -0400

    added colors.dart file

---
+++ lib/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+Color primaryTextColor = Color(0xFF414C6B);
+Color secondaryTextColor = Color(0xFFE4979E);
+Color titleTextColor = Colors.green.shade900;
+Color titleTextColorOne = Colors.white;
+Color titleTextColorTwo = Colors.black;
+Color contentTextColor = Color(0xff868686);
+Color navigationColor = Color(0xFF6751B5);
+Color gradientStartColor = Color(0xFF0050AC);
+Color gradientEndColor = Color(0xFF9354B9);
