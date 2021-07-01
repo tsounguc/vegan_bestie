995231699020d6a2594db7b031fd15a627c6bfc2
Christian Tsoungui Nkoulou <ChristianN@ruthsx.com>
2021-07-01 01:21:51 -0400

    working on being able to upload image into the database

--- lib/addProduct/widgets/add_product_form.dart
+++ lib/addProduct/widgets/add_product_form.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:sheveegan/colors.dart';
 import 'package:sheveegan/productprovider.dart';
 import 'package:sheveegan/widgets/input_text_field.dart';
 
@@ -20,6 +21,10 @@ class AddProductForm extends HookWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
+            // Expanded(child: imageProfile()),
+            SizedBox(
+              height: 50,
+            ),
             InputTextFormField(
               initialValue: productScanResults.barcode,
               labelText: 'Barcode',
@@ -71,21 +76,19 @@ class AddProductForm extends HookWidget {
                 "Submit",
                 style: TextStyle(),
               ),
-              onPressed: (){
-                if (!_formKey.currentState!.validate())
-                  {
-                    //
-                    print("Please enter info");
-                  }else{
+              onPressed: () {
+                if (!_formKey.currentState!.validate()) {
+                  //
+                  print("Please enter info");
+                } else {
                   _formKey.currentState!.save();
                   print(barcode);
                   print(productName);
                   print(ingredients);
-                  context.read(productProvider).addNewProduct(barcode!, productName!, ingredients!);
+                  context
+                      .read(productProvider)
+                      .addNewProduct(barcode!, productName!, ingredients!);
                 }
-
-
-
               },
             ),
           ],
@@ -94,58 +97,30 @@ class AddProductForm extends HookWidget {
     );
   }
 
-  Widget _buildBarcode(BuildContext context) {
-    return InputTextFormField(
-      initialValue: context.read(productProvider).barcode,
-      labelText: 'Barcode',
-      // hintText: 'Enter Barcode',
-      validator: (String? value) {
-        if (value!.isEmpty) {
-          return 'Barcode is required';
-        }
-      },
-      onSaved: (String? value) {
-        barcode = value;
-      },
-    );
-  }
-
-  Widget _buildProductName(BuildContext context) {
-    return InputTextFormField(
-      initialValue: context.read(productProvider).productName,
-      // != null ? context.read(productProvider).productName : "",
-      labelText: 'Product Name',
-      // hintText: 'Enter Product Name',
-      validator: (String? value) {
-        if (value!.isEmpty) {
-          return 'Product Name is required';
-        }
-      },
-      onSaved: (String? value) {
-        productName = value;
-      },
-    );
-  }
-
-  Widget _buildIngredients(BuildContext context) {
-    return InputTextFormField(
-      initialValue: context.read(productProvider).ingredientsText != null
-          ? context.read(productProvider).ingredientsText
-          : "",
-      labelText: 'Ingredients',
-      // hintText: "Enter Ingredients list",
-      minLines: 5,
-      border: OutlineInputBorder(
-        borderSide: BorderSide(),
-      ),
-      validator: (String? value) {
-        if (value!.isEmpty) {
-          return 'Ingredients is required';
-        }
-      },
-      onSaved: (String? value) {
-        ingredients = value;
-      },
+  Widget imageProfile() {
+    return Stack(
+      children: [
+        CircleAvatar(
+          radius: 80,
+          backgroundColor: gradientStartColor,
+          child: Icon(
+            Icons.image_outlined,
+            color: Colors.white,
+            size: 100,
+          ),
+        ),
+        Positioned(
+          bottom: 5.0,
+          right: 0,
+          child: IconButton(iconSize: 30,
+            icon: Icon(
+              Icons.camera_alt,
+              color: Colors.black,
+            ),
+            onPressed: () {},
+          ),
+        )
+      ],
     );
   }
 }
--- lib/she_vegan_home_page.dart
+++ lib/she_vegan_home_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:sheveegan/addProduct/add_product.dart';
 import 'package:sheveegan/error_state_page.dart';
 import 'package:sheveegan/product_found.dart';
 import 'package:sheveegan/loading_state_page.dart';
@@ -22,5 +23,6 @@ class SheVeganHomePage extends HookWidget {
     }
 
     return ProductFoundPage();
+    // return AddProduct();
   }
 }
