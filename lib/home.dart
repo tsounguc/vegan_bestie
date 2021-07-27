e6839099acdf2fcef571df7e28e0beb25ca0b020
tsounguc <tsounguc@mail.gvus.edu>
2021-07-26 23:38:09 -0400

    removed imported packages from main and home that aren't used

--- lib/home.dart
+++ lib/home.dart
@@ -1,28 +1,26 @@
-import 'package:camera_platform_interface/src/types/camera_description.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:sheveegan/she_vegan_home_page.dart';
 
 class Home extends HookWidget {
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "SheVegan Home",
       theme: ThemeData(
-        primaryColor: Colors.green.shade50,
-        primaryColorDark: Colors.green,
-        primaryColorLight: Colors.lime,
-        textTheme: TextTheme(headline1: TextStyle(
-          color: Colors.black,
-          fontSize: 27,
-          fontWeight: FontWeight.bold,
-          fontFamily: 'cursive',
-        ),)
-      ),
+          primaryColor: Colors.green.shade50,
+          primaryColorDark: Colors.green,
+          primaryColorLight: Colors.lime,
+          textTheme: TextTheme(
+            headline1: TextStyle(
+              color: Colors.black,
+              fontSize: 27,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'cursive',
+            ),
+          )),
       home: SheVeganHomePage(),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:sheveegan/home.dart';
