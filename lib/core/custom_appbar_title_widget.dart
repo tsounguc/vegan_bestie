c411ebc15725dd43f0bdb1922160aa82de4d2227
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-04-13 00:09:11 -0400

    fixed more bug fixes

--- lib/core/custom_appbar_title_widget.dart
+++ lib/core/custom_appbar_title_widget.dart
@@ -13,27 +13,29 @@ class CustomAppbarTitleWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       width: MediaQuery.of(context).size.width * 0.5,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image.asset(
-            imageOneName,
-            fit: BoxFit.contain,
-            height: MediaQuery.of(context).size.width * 0.07,
-            width: MediaQuery.of(context).size.width * 0.07,
-          ),
-          Text(
-            Strings.appTitle,
-            style: Theme.of(context).textTheme.titleLarge,
-          ),
-          Image.asset(
-            imageTwoName,
-            fit: BoxFit.contain,
-            height: MediaQuery.of(context).size.width * 0.07,
-            width: MediaQuery.of(context).size.width * 0.07,
-          ),
-        ],
+      child: FittedBox(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              imageOneName,
+              fit: BoxFit.contain,
+              height: MediaQuery.of(context).size.width * 0.07,
+              width: MediaQuery.of(context).size.width * 0.07,
+            ),
+            Text(
+              Strings.appTitle,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            Image.asset(
+              imageTwoName,
+              fit: BoxFit.contain,
+              height: MediaQuery.of(context).size.width * 0.07,
+              width: MediaQuery.of(context).size.width * 0.07,
+            ),
+          ],
+        ),
       ),
     );
   }
