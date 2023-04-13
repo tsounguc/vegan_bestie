ee26fd62bc9e83a8d2a93f5cb8720b5396b2121c
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-04-13 00:02:56 -0400

    centered appbar title some more

--- lib/core/custom_appbar_title_widget.dart
+++ lib/core/custom_appbar_title_widget.dart
@@ -11,35 +11,30 @@ class CustomAppbarTitleWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Image.asset(
-          imageOneName,
-          fit: BoxFit.contain,
-          height: MediaQuery.of(context).size.width * 0.07,
-          width: MediaQuery.of(context).size.width * 0.07,
-        ),
-        SizedBox(
-          width: MediaQuery.of(context).size.width * 0.40,
-          child: Center(
-            child: Text(
-              Strings.appTitle,
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.5,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset(
+            imageOneName,
+            fit: BoxFit.contain,
+            height: MediaQuery.of(context).size.width * 0.07,
+            width: MediaQuery.of(context).size.width * 0.07,
           ),
-        ),
-        SizedBox(
-          width: MediaQuery.of(context).size.width * 0.003,
-        ),
-        Image.asset(
-          imageTwoName,
-          fit: BoxFit.contain,
-          height: MediaQuery.of(context).size.width * 0.07,
-          width: MediaQuery.of(context).size.width * 0.07,
-        ),
-      ],
+          Text(
+            Strings.appTitle,
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+          Image.asset(
+            imageTwoName,
+            fit: BoxFit.contain,
+            height: MediaQuery.of(context).size.width * 0.07,
+            width: MediaQuery.of(context).size.width * 0.07,
+          ),
+        ],
+      ),
     );
   }
 }
