6d8aea4fa1bbd698ad274dbe610b3bb90703cd55
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-05-01 14:30:13 -0400

    map now loads correct user location and restaurants near by when user changes location

--- lib/features/restaurants/presentation/pages/map_page.dart
+++ lib/features/restaurants/presentation/pages/map_page.dart
@@ -2,10 +2,12 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:sheveegan/core/error.dart';
 
 import '../../../../core/loading.dart';
+import '../geolocation_bloc/geolocation_bloc.dart';
 import '../map_cubit/map_cubit.dart';
 
 class MapPage extends StatefulWidget {
@@ -17,6 +19,7 @@ class MapPage extends StatefulWidget {
 
 class _MapPageState extends State<MapPage> {
   GoogleMapController? _controller;
+  Position? userCurrentLocation;
 
   @override
   void dispose() {
@@ -24,13 +27,9 @@ class _MapPageState extends State<MapPage> {
     _controller?.dispose();
   }
 
-  static final CameraPosition _kGooglePlex =
-      CameraPosition(target: LatLng(37.4279, -122.0857), zoom: 14.4746);
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334,
-      target: LatLng(37.4329, -122.0883),
-      tilt: 59.4407,
-      zoom: 19.1519);
+  static final CameraPosition _kGooglePlex = CameraPosition(target: LatLng(37.4279, -122.0857), zoom: 14.4746);
+  static final CameraPosition _kLake =
+      CameraPosition(bearing: 192.8334, target: LatLng(37.4329, -122.0883), tilt: 59.4407, zoom: 19.1519);
 
   @override
   Widget build(BuildContext context) {
@@ -43,17 +42,19 @@ class _MapPageState extends State<MapPage> {
           debugPrint(state.markers.first.mapsId.value);
           return GoogleMap(
             myLocationEnabled: true,
+            myLocationButtonEnabled: false,
             mapType: MapType.normal,
             markers: state.markers,
+            // trafficEnabled: true,
+            // cameraTargetBounds: CameraTargetBounds(
+            //   MapUtils.boundsFromLatLngList(state.markers.map((location) => location.position).toList()),
+            // ),
             initialCameraPosition: CameraPosition(
-              bearing: 190,
-              target: LatLng(
-                  state.userLocation.latitude, state.userLocation.longitude),
-              zoom: 11.4746,
+              // bearing: 190,
+              target: LatLng(state.userLocation.latitude, state.userLocation.longitude),
+              // zoom: 11.4746,
             ),
-            // initialCameraPosition: _kGooglePlex,
-            onMapCreated: (GoogleMapController controller) =>
-                onMapCreated(controller, state),
+            onMapCreated: (GoogleMapController controller) => _onMapCreated(controller, state),
           );
         } else {
           return LoadingPage();
@@ -62,14 +63,37 @@ class _MapPageState extends State<MapPage> {
     );
   }
 
-  void onMapCreated(
-      GoogleMapController controller, MapLocationsFound state) async {
+  void _onMapCreated(GoogleMapController controller, MapLocationsFound state) async {
     _controller = controller;
-    //   CameraPosition(
-    //         bearing: 190,
-    //         target: LatLng(state.userLocation.latitude, state.userLocation.longitude),
-    //         zoom: 11.4746,
-    //       )
-    // _controller.animateCamera(cameraUpdate)
+    userCurrentLocation = BlocProvider.of<GeolocationBloc>(context).currentLocation;
+
+    Future.delayed(
+      Duration(milliseconds: 200),
+      () => _controller?.animateCamera(
+        CameraUpdate.newLatLngBounds(
+          MapUtils.boundsFromLatLngList(state.markers.map((location) => location.position).toList()),
+          1,
+        ),
+      ),
+    );
+  }
+}
+
+//https://www.appsloveworld.com/flutter/100/47/keep-all-markers-in-view-with-flutter-google-maps-plugin
+class MapUtils {
+  static LatLngBounds boundsFromLatLngList(List<LatLng> list) {
+    double? x0, x1, y0, y1;
+    for (LatLng latLng in list) {
+      if (x0 == null) {
+        x0 = x1 = latLng.latitude;
+        y0 = y1 = latLng.longitude;
+      } else {
+        if (latLng.latitude > x1!) x1 = latLng.latitude;
+        if (latLng.latitude < x0) x0 = latLng.latitude;
+        if (latLng.longitude > y1!) y1 = latLng.longitude;
+        if (latLng.longitude < y0!) y0 = latLng.longitude;
+      }
+    }
+    return LatLngBounds(northeast: LatLng(x1! + 0.005, y1! + 0.005), southwest: LatLng(x0! - 0.005, y0! - 0.005));
   }
 }
