d6902f07bd51ebf21211b0ef8a087ad516174a36
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-02-24 00:30:04 -0500

    All tests passes

--- lib/features/scan_product/data/mapper/scan_barcode_mapper.dart
+++
@@ -1,8 +0,0 @@
-import '../../domain/entities/barcode.dart';
-import '../models/barcode_model.dart';
-
-class ScanBarcodeMapper {
-  Barcode mapToEntity(BarcodeModel barcodeModel) {
-    return Barcode(barcode: barcodeModel.barcode);
-  }
-}
