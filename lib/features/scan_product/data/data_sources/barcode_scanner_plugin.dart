ba01fe6e50e50506d6ccb0da27dd8dab682b3ed2
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-02-26 00:41:28 -0500

    Move barcode_scanner_plugin.dart to data_sources.

---
+++ lib/features/scan_product/data/data_sources/barcode_scanner_plugin.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:sheveegan/core/failures_successes/exceptions.dart';
+
+// abstract class BarcodeScannerService {
+//   Future<String> scanBarcode();
+// }
+
+class BarcodeScannerService {
+  Future<String> scanBarcode() async {
+    final barcodeString = await FlutterBarcodeScanner.scanBarcode(
+      '#ff6666',
+      'Cancel',
+      true,
+      ScanMode.DEFAULT,
+    );
+    debugPrint('Barcode: $barcodeString');
+    if (barcodeString == '-1') {
+      throw const ScanException(message: 'Scan Cancelled');
+    }
+    if (int.tryParse(barcodeString) == null ||
+        barcodeString.isEmpty ||
+        barcodeString.length < 12 ||
+        barcodeString.length > 13) {
+      debugPrint('Invalid Barcode');
+      throw const ScanException(message: 'Invalid Barcode');
+    }
+
+    return barcodeString;
+  }
+}
