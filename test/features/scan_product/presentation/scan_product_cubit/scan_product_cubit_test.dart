bc8fb510c986dc37dddc46c98fa9f3f326cf4e94
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-02-27 09:06:19 -0500

    Tests for scanBarcode in scan product cubit.All Tests pass

--- test/features/scan_product/presentation/scan_product_cubit/scan_product_cubit_test.dart
+++ test/features/scan_product/presentation/scan_product_cubit/scan_product_cubit_test.dart
@@ -1,5 +1,9 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:sheveegan/core/failures_successes/failures.dart';
+import 'package:sheveegan/features/scan_product/domain/entities/barcode.dart';
 import 'package:sheveegan/features/scan_product/domain/use_cases/fetch_product.dart';
 import 'package:sheveegan/features/scan_product/domain/use_cases/scan_barcode.dart';
 import 'package:sheveegan/features/scan_product/presentation/scan_product_cubit/scan_product_cubit.dart';
@@ -31,5 +35,50 @@ void main() {
     // Act
     // Assert
     expect(cubit.state, const ScanProductInitial());
+  });
+
+  group('scanBarcode - ', () {
+    const testBarcode = Barcode(barcode: '123456789012');
+    const testScanFailure = ScanFailure(message: 'message', statusCode: 400);
+    blocTest<ScanProductCubit, ScanProductState>(
+      'given ScanProductCubit '
+      'when [ScanProductCubit.scanBarcode] call completed successfully '
+      'then emit [ScanningBarcode, BarcodeFound] ',
+      build: () {
+        when(() => scanBarcode()).thenAnswer(
+          (_) async => const Right(testBarcode),
+        );
+        return cubit;
+      },
+      act: (cubit) => cubit.scanBarcode(),
+      expect: () => [
+        const ScanningBarcode(),
+        BarcodeFound(barcode: testBarcode.barcode),
+      ],
+      verify: (cubit) {
+        verify(() => scanBarcode()).called(1);
+        verifyNoMoreInteractions(scanBarcode);
+      },
+    );
+    blocTest<ScanProductCubit, ScanProductState>(
+      'given ScanProductCubit '
+      'when [ScanProductCubit.scanBarcode] call unsuccessful '
+      'then emit [ScanningBarcode, ScanProductError] ',
+      build: () {
+        when(() => scanBarcode()).thenAnswer(
+          (_) async => const Left(testScanFailure),
+        );
+        return cubit;
+      },
+      act: (cubit) => cubit.scanBarcode(),
+      expect: () => [
+        const ScanningBarcode(),
+        ScanProductError(message: testScanFailure.errorMessage),
+      ],
+      verify: (cubit) {
+        verify(() => scanBarcode()).called(1);
+        verifyNoMoreInteractions(scanBarcode);
+      },
+    );
   });
 }
