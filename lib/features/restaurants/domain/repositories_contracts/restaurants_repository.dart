f40007d08c2f6f83bb95d833679d3a4a4e6096ee
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-03-03 18:39:30 -0500

    Rewrite restaurant repository in domain layer

---
+++ lib/features/restaurants/domain/repositories_contracts/restaurants_repository.dart
@@ -0,0 +1,10 @@
+import 'package:geolocator/geolocator.dart';
+import 'package:sheveegan/core/utils/typedefs.dart';
+import 'package:sheveegan/features/restaurants/domain/entities/restaurant.dart';
+import 'package:sheveegan/features/restaurants/domain/entities/restaurant_details.dart';
+
+abstract class RestaurantsRepositoryContract {
+  ResultFuture<List<Restaurant>> getRestaurantsNearMe(Position position);
+
+  ResultFuture<RestaurantDetails> getRestaurantDetail(String id);
+}
