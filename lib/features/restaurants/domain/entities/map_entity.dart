0c0090088bec1b67f8bc70a09cb6311ead942fa5
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-03-06 21:35:57 -0500

    Rewrite map entity

--- lib/features/restaurants/domain/entities/map_entity.dart
+++ lib/features/restaurants/domain/entities/map_entity.dart
@@ -1,7 +1,29 @@
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapEntity {
-  Set<Marker>? restaurantsMarkers;
+  const MapEntity({
+    required this.markers,
+    required this.userLocation,
+  });
 
-  MapEntity({this.restaurantsMarkers});
+  MapEntity.empty()
+      : this(
+          markers: {},
+          userLocation: Position(
+            longitude: 0,
+            latitude: 0,
+            timestamp: DateTime.now(),
+            accuracy: 0,
+            altitude: 0,
+            altitudeAccuracy: 0,
+            heading: 0,
+            headingAccuracy: 0,
+            speed: 0,
+            speedAccuracy: 0,
+          ),
+        );
+
+  final Set<Marker> markers;
+  final Position userLocation;
 }
