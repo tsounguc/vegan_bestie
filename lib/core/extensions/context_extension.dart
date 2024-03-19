f356712be45284d1274bb371682032d5a82ba382
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-03-19 11:16:53 -0400

    Create extentions for userProvider and currentUser

--- lib/core/extensions/context_extension.dart
+++ lib/core/extensions/context_extension.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sheveegan/core/common/app/providers/user_provider.dart';
+import 'package:sheveegan/features/auth/domain/entities/user_entity.dart';
 
 extension ContextExtension on BuildContext {
   ThemeData get theme => Theme.of(this);
+
   MediaQueryData get mediaQuery => MediaQuery.of(this);
+
   Size get size => mediaQuery.size;
+
   double get width => size.width;
+
   double get height => size.height;
+
+  UserProvider get userProvider => read<UserProvider>();
+
+  UserEntity? get currentUser => userProvider.user;
 }
