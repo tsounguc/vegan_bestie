a02037b5756d1fa8826f0feef2602d9211faca27
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-04-10 11:21:14 -0400

    Create SectionHeader widget

---
+++ lib/core/common/widgets/section_header.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:sheveegan/core/extensions/context_extension.dart';
+
+class SectionHeader extends StatelessWidget {
+  const SectionHeader({
+    required this.sectionTitle,
+    required this.seeAll,
+    required this.onSeeAll,
+    super.key,
+  });
+
+  final String sectionTitle;
+  final bool seeAll;
+  final VoidCallback onSeeAll;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          sectionTitle,
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        if (seeAll)
+          TextButton(
+            style: TextButton.styleFrom(
+              padding: EdgeInsets.zero,
+              splashFactory: NoSplash.splashFactory,
+            ),
+            onPressed: onSeeAll,
+            child: Text(
+              'See All',
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                color: context.theme.primaryColor,
+              ),
+            ),
+          )
+      ],
+    );
+  }
+}
